605461b21e54808f45a6aec9a0bc44aca1027642
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-09-11 05:28:58 -0700

    updated
    i have already made the code but now i am only pasting it in  new project after full completion  the plugin name was (flutter_staggered_grid_view: ^0.7.0)

---
+++ lib/product_grid/product_grid.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'product_model.dart';
+import 'product_detail.dart';
+
+class ProductGrid extends StatelessWidget {
+  final List<Product> products;
+
+  ProductGrid({required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 4,
+      itemCount: products.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ProductCard(product: products[index]);
+      },
+      staggeredTileBuilder: (int index) => StaggeredTile.fit(2),
+      mainAxisSpacing: 8.0,
+      crossAxisSpacing: 8.0,
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+
+  ProductCard({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProductDetail(product: product)),
+        );
+      },
+      child: Card(
+        elevation: 4,
+        child: Column(
+          children: [
+            Image.network(product.imageUrl, fit: BoxFit.cover),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text('\$${product.price}', style: TextStyle(color: Colors.green)),
+          ],
+        ),
+      ),
+    );
+  }
+}
